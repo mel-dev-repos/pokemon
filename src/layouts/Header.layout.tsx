8689cdff1b06433682d8dec3a000ede99ad5cc54
mel-dev-repos <musakhani1379@gmail.com>
2023-11-10 00:09:03 +0330

    fix: Show Pokemon Selected In Header

--- src/layouts/Header.layout.tsx
+++ src/layouts/Header.layout.tsx
@@ -1,9 +1,22 @@
 import { CubeIcon } from "@heroicons/react/24/outline";
 import { PATHS } from "configs";
+import { usePokemonStore } from "hooks";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { checkPokemonDetailsIsEmpty } from "utils";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const { pokemonDetails } = usePokemonStore((state) => state);
+  const [lastPokemon, setLastPokemon] = useState<string>("");
+
+  useEffect(() => {
+    if(checkPokemonDetailsIsEmpty(pokemonDetails)) {
+      setLastPokemon("");
+    } else {
+      setLastPokemon(pokemonDetails.name);
+    }
+  })
 
   return (
     <header className="flex flex-col sm:flex-row items-center justify-between p-4 bg-gray-800 text-white min-h-[10vh]">
@@ -14,7 +27,7 @@ export const Header = () => {
       <nav className="mt-4 sm:mt-0">
         <ul className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 text-center">
           <li className="hover:text-gray-300 cursor-pointer" onClick={() => navigate(PATHS.POKEMON_DETAILS)}>
-            Last Item
+            Last Item {lastPokemon !== "" ? `(${lastPokemon})` : ""}
           </li>
           <li className="hover:text-gray-300 cursor-pointer" onClick={() => navigate(PATHS.ABOUT_US)}>
             About Us
