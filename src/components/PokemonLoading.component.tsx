54c03d9de33bdc4f5b8a53b96992d50cf7e9f224
mel-dev-repos <musakhani1379@gmail.com>
2023-11-09 22:56:14 +0330

    feat: Create PokemonLoading Component

--- src/components/PokemonLoading.component.tsx
+++ src/components/PokemonLoading.component.tsx
@@ -1,7 +1,16 @@
 import "assets/styles/components/PokemonLoading.style.css";
+const PokemonLogo = require("assets/images/pokemon-logo.png");
 
 export const PokemonLoading = () => {
   return (
-    <p>PokemonLoading</p>
+    <div className="flex items-center justify-center h-[60vh] m-[-15px] text-center">
+      <img
+        className="w-20 h-20 animate-spin"
+        src={PokemonLogo}
+        alt=""
+        width="120"
+        height="120"
+      />
+    </div>
   )
 }
