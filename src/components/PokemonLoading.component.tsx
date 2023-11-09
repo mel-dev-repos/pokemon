f1d7641de51a7ff745ee97f35cb9f090340c65e4
mel-dev-repos <musakhani1379@gmail.com>
2023-11-10 00:25:13 +0330

    fix: Change Loading Component Style

--- src/components/PokemonLoading.component.tsx
+++ src/components/PokemonLoading.component.tsx
@@ -3,7 +3,7 @@ const PokemonLogo = require("assets/images/pokemon-logo.png");
 
 export const PokemonLoading = () => {
   return (
-    <div className="flex items-center justify-center h-[60vh] m-[-15px] text-center">
+    <div className="flex items-center justify-center h-[60vh] m-[-15px] text-center w-[100%]">
       <img
         className="w-20 h-20 animate-spin"
         src={PokemonLogo}
